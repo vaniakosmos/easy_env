d9903681156a2ba32d5546842052ab5a1bbd5afb
Bachynin Ivan <bachynin.i@gmail.com>
2018-07-07 16:57:30 +0300

    ✅ add tests

--- easy_env/getters.py
+++ easy_env/getters.py
@@ -1,15 +1,17 @@
 import os
-
-from typing import Optional, Union
+from typing import Callable, Optional, TypeVar, Union
 
 
 __all__ = [
     'get_int', 'get_float', 'get_bool', 'get_str', 'get_bytes', 'get_list', 'get',
 ]
-TRUE_VALUES = set('1 y yes ok okay confirm absolutely totally yep'.split())
+TRUE_VALUES = set('1 true t yes y ok okay confirm absolutely totally yep'.split())
+FALSE_VALUES = set("0 false f no n bad nope don't".split())
+T = TypeVar('T')
 
 
-def process(factory, key: str, default: Optional[int], raise_error: bool):
+def process(factory: Callable[[str], T], key: str, default: Optional[T],
+            raise_error: bool) -> Optional[T]:
     value = os.getenv(key, None)
     if value is None:
         if default is not None:
@@ -21,11 +23,15 @@ def process(factory, key: str, default: Optional[int], raise_error: bool):
     return value
 
 
-def bool_factory(value: str):
-    return value in TRUE_VALUES
+def bool_factory(value: str) -> bool:
+    if value in TRUE_VALUES:
+        return True
+    if value in FALSE_VALUES:
+        return False
+    raise ValueError("Bad boolean value.")
 
 
-def bytes_factory(value: str, encoding: str):
+def bytes_factory(value: str, encoding: str) -> bytes:
     return value.encode(encoding)
 
 
@@ -33,52 +39,56 @@ def list_factory(value: str, separator: str, item_factory):
     return list(map(item_factory, value.split(separator)))
 
 
-def get_int(key: str, default: Optional[int] = None, raise_error=False) -> Optional[int]:
+def get_int(key: str, default: Optional[int] = None, raise_error=False, **_) -> Optional[int]:
     return process(int, key, default, raise_error)
 
 
-def get_float(key: str, default: Optional[float] = None, raise_error=False):
+def get_float(key: str, default: Optional[float] = None, raise_error=False, **_) -> Optional[float]:
     return process(float, key, default, raise_error)
 
 
-def get_str(key: str, default: Optional[str] = None, raise_error=False):
-    return process(str, key, default, raise_error)
+def get_str(key: str, default: Optional[str] = None, raise_error=False, **_) -> Optional[str]:
+    return process(lambda x: str(x), key, default, raise_error)
 
 
-def get_bool(key: str, default: Optional[bool] = None, raise_error=False):
-    return process(str, key, default, raise_error)
+def get_bool(key: str, default: Optional[bool] = None, raise_error=False, **_) -> Optional[bool]:
+    return process(bool_factory, key, default, raise_error)
 
 
-def get_bytes(key: str, default: Optional[int] = None, raise_error=False, encoding='utf-8'):
+def get_bytes(key: str, default: Optional[bytes] = None, raise_error=False, **kwargs) -> Optional[bytes]:
+    encoding = kwargs.pop('encoding', 'utf-8')
     return process(lambda x: bytes_factory(x, encoding),
                    key, default, raise_error)
 
 
-def get_list(key: str, default: Optional[int] = None, raise_error=False, separator=',', item_factory=str):
+def get_list(key: str, default: Optional[list] = None, raise_error=False, **kwargs) -> Optional[list]:
+    separator = kwargs.pop('separator', ',')
+    item_factory = kwargs.pop('item_factory', str)
     return process(lambda x: list_factory(x, separator, item_factory),
                    key, default, raise_error)
 
 
 def based_on_default(key: str, default: Optional[int] = None,
-                     raise_error=False, separator=',', item_factory=str, encoding='utf-8'):
+                     raise_error=False, **kwargs):
+    type_mapper = {
+        int: get_int,
+        float: get_float,
+        str: get_str,
+        bool: get_bool,
+        bytes: get_bytes,
+        list: get_list,
+    }
     default_type = type(default)
-    if default_type is int:
-        return get_int(key, default, raise_error)
-    if default_type is float:
-        return get_float(key, default, raise_error)
-    if default_type is str:
-        return get_str(key, default, raise_error)
-    if default_type is bool:
-        return get_bool(key, default, raise_error)
-    if default_type is bytes:
-        return get_bytes(key, default, raise_error, encoding)
-    if default_type is list:
-        return get_list(key, default, raise_error, separator, item_factory)
+    if default_type in type_mapper:
+        getter = type_mapper[default_type]
+        return getter(key, default, raise_error, **kwargs)
+    raise ValueError("Unknown type of default value.")
 
 
 def get(key: str, default: Optional[Union[int, float, bool, str, bytes, list]] = None,
         raise_error=False, separator=',', item_factory=str, encoding='utf-8'):
     """Autodetect right type based on default."""
     if default is not None:
-        return based_on_default(key, default, raise_error, separator, item_factory, encoding)
+        return based_on_default(key, default, raise_error, separator=separator,
+                                item_factory=item_factory, encoding=encoding)
     return get_str(key, default, raise_error)
--- tests/test_getters.py
+++ tests/test_getters.py
@@ -4,28 +4,204 @@ from unittest import TestCase
 import easy_env as environ
 
 
-class IntGetterTests(TestCase):
+class IntTests(TestCase):
     def setUp(self):
         os.environ.clear()
 
     def test_get(self):
-        environ.set_int('INT_VAL', 1)
-        value = environ.get_int('INT_VAL')
+        os.environ['VALUE'] = '1'
+        value = environ.get_int('VALUE')
         self.assertEqual(value, 1)
 
     def test_get_default(self):
-        value = environ.get_int('INT_VAL', 2)
+        value = environ.get_int('VALUE', 2)
         self.assertEqual(value, 2)
 
     def test_none(self):
-        value = environ.get_int('INT_VAL')
+        value = environ.get_int('VALUE')
         self.assertIsNone(value)
 
     def test_raise_error(self):
         with self.assertRaises(KeyError):
-            environ.get_int('INT_VAL', raise_error=True)
+            environ.get_int('VALUE', raise_error=True)
 
     def test_bad_value(self):
-        os.environ['INT_VAL'] = 'not int'
+        os.environ['VALUE'] = 'not int'
         with self.assertRaises(ValueError):
-            environ.get_int('INT_VAL')
+            environ.get_int('VALUE')
+
+
+class FloatTests(TestCase):
+    def setUp(self):
+        os.environ.clear()
+
+    def test_get(self):
+        os.environ['VALUE'] = '1.1'
+        value = environ.get_float('VALUE')
+        self.assertAlmostEqual(value, 1.1)
+
+    def test_get_int(self):
+        os.environ['VALUE'] = '1'
+        value = environ.get_float('VALUE')
+        self.assertAlmostEqual(value, 1.0)
+
+    def test_get_default(self):
+        value = environ.get_float('VALUE', 2.25)
+        self.assertAlmostEqual(value, 2.25)
+
+    def test_none(self):
+        value = environ.get_float('VALUE')
+        self.assertIsNone(value)
+
+    def test_raise_error(self):
+        with self.assertRaises(KeyError):
+            environ.get_float('VALUE', raise_error=True)
+
+    def test_bad_value(self):
+        os.environ['VALUE'] = 'not float'
+        with self.assertRaises(ValueError):
+            environ.get_float('VALUE')
+
+
+class BoolTests(TestCase):
+    def setUp(self):
+        os.environ.clear()
+
+    def test_get(self):
+        os.environ['VALUE'] = '1'
+        value = environ.get_bool('VALUE')
+        self.assertTrue(value)
+        os.environ['VALUE'] = '0'
+        value = environ.get_bool('VALUE')
+        self.assertFalse(value)
+
+    def test_get_default(self):
+        value = environ.get_bool('VALUE', True)
+        self.assertTrue(value)
+        value = environ.get_bool('VALUE', False)
+        self.assertFalse(value)
+
+    def test_none(self):
+        value = environ.get_bool('VALUE')
+        self.assertIsNone(value)
+
+    def test_raise_error(self):
+        with self.assertRaises(KeyError):
+            environ.get_bool('VALUE', raise_error=True)
+
+    def test_bad_value(self):
+        os.environ['VALUE'] = 'not bool'
+        with self.assertRaises(ValueError):
+            environ.get_bool('VALUE')
+
+
+class StrTests(TestCase):
+    def setUp(self):
+        os.environ.clear()
+
+    def test_get(self):
+        os.environ['VALUE'] = '1'
+        value = environ.get_str('VALUE')
+        self.assertEqual(value, '1')
+
+    def test_get_default(self):
+        value = environ.get_str('VALUE', 'foo bar')
+        self.assertEqual(value, 'foo bar')
+
+    def test_none(self):
+        value = environ.get_str('VALUE')
+        self.assertIsNone(value)
+
+    def test_raise_error(self):
+        with self.assertRaises(KeyError):
+            environ.get_str('VALUE', raise_error=True)
+
+
+class BytesTests(TestCase):
+    def setUp(self):
+        os.environ.clear()
+
+    def test_get(self):
+        os.environ['VALUE'] = '🔥'
+        value = environ.get_bytes('VALUE')
+        self.assertEqual(value, '🔥'.encode())
+
+    def test_cant_decode(self):
+        os.environ['VALUE'] = '🔥'
+        with self.assertRaises(UnicodeEncodeError):
+            value = environ.get_bytes('VALUE', encoding='ascii')
+
+    def test_get_default(self):
+        value = environ.get_bytes('VALUE', b'yes')
+        self.assertEqual(value, b'yes')
+
+    def test_none(self):
+        value = environ.get_bytes('VALUE')
+        self.assertIsNone(value)
+
+    def test_raise_error(self):
+        with self.assertRaises(KeyError):
+            environ.get_bytes('VALUE', raise_error=True)
+
+
+class ListTests(TestCase):
+    def setUp(self):
+        os.environ.clear()
+
+    def test_get(self):
+        os.environ['VALUE'] = '1,2'
+        value = environ.get_list('VALUE')
+        self.assertEqual(value, ['1', '2'])
+        os.environ['VALUE'] = '1,2'
+        value = environ.get_list('VALUE', item_factory=int)
+        self.assertEqual(value, [1, 2])
+
+    def test_another_separator(self):
+        os.environ['VALUE'] = '1 2'
+        value = environ.get_list('VALUE', separator=' ')
+        self.assertEqual(value, ['1', '2'])
+
+    def test_get_default(self):
+        value = environ.get_list('VALUE', [1, 2])
+        self.assertEqual(value, [1, 2])
+
+    def test_none(self):
+        value = environ.get_list('VALUE')
+        self.assertIsNone(value)
+
+    def test_raise_error(self):
+        with self.assertRaises(KeyError):
+            environ.get_list('VALUE', raise_error=True)
+
+    def test_bad_value(self):
+        os.environ['VALUE'] = '1,,2'
+        with self.assertRaises(ValueError):
+            environ.get_list('VALUE', item_factory=int)
+
+
+class AutoGetterTests(TestCase):
+    def setUp(self):
+        os.environ.clear()
+
+    def test_bad_default(self):
+        with self.assertRaises(ValueError):
+            environ.get('VALUE', default=object())
+
+    def test_without_default(self):
+        os.environ['VALUE'] = '432'
+        value = environ.get('VALUE')
+        self.assertEqual(value, '432')
+
+    def test_get_int(self):
+        os.environ['VALUE'] = '5'
+        value = environ.get('VALUE', 1)
+        self.assertEqual(value, 5)
+
+    def test_get_int_default(self):
+        value = environ.get('VALUE', 1)
+        self.assertEqual(value, 1)
+
+    def test_get_list(self):
+        os.environ['VALUE'] = '1,2,4'
+        value = environ.get('VALUE', [], item_factory=int)
+        self.assertEqual(value, [1, 2, 4])
